0064b91e72f2061b354f8c590a7cf299545516e6
OuyangLingxin <ouyangmayi@gmail.com>
2023-08-25 22:59:54 -0400

    saving project codes

---
+++ model.py
@@ -0,0 +1,94 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Employee(db.Model):
+    __tablename__ = 'employees'
+    employee_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    employee_email = db.Column(db.String)
+    employee_password = db.Column(db.String)
+    employee_name = db.Column(db.String)
+    employee_cell = db.Column(db.String)
+
+    address = db.relationship('Address', back_populates='employee')
+    order = db.relationship('Order', back_populates='employee')
+    warehouse = db.relationship('Warehouse', back_populates='employee')
+
+    def __repr__(self):
+        return f"<Employee employee_id={self.employee_id} employee_name={self.employee_name}>"
+
+class Address(db.Model):
+    __tablename__ = 'addresses'
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), autoincrement=True, primary_key=True)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
+    employee = db.relationship('Employee', back_populates='address')
+
+    def __repr__(self):
+        return f"<Address employee_id={self.employee_id} lat={self.lat} lng={self.lng}>"
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    order_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    order_status = db.Column(db.String)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'))
+    warehouse_id = db.Column(db.Integer)
+
+    employee = db.relationship('Employee', back_populates='order')
+    warehouse = db.relationship('Warehouse', back_populates='order')
+
+    def __repr__(self):
+        return f"<Order order_id={self.order_id} delivery_address={self.delivery_address}>"
+
+class Warehouse(db.Model):
+    __tablename__ = 'warehouses'
+    warehouse_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'))
+    warehouse_location = db.Column(db.String)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
+    employee = db.relationship('Employee', back_populates='warehouse')
+    order = db.relationship('Order', back_populates='warehouse')
+
+    def __repr__(self):
+        return f"<Warehouse warehouse_id={self.warehouse_id} warehouse_location={self.warehouse_location}>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///mapin", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
---
+++ seed.py
@@ -0,0 +1,72 @@
+import os
+import server
+import model
+from datetime import datetime
+from randomuser import RandomUser
+import random_address
+import json
+
+os.system("dropdb mapin")
+os.system("createdb mapin")
+model.connect_to_db(server.app)
+model.db.create_all()
+
+userList = RandomUser.generate_users(3, {'nat':'us'})
+
+with open('data/order.json') as o:
+    order_data = json.loads(o.read())
+
+with open('data/warehouse.json') as w:
+    warehouse_data = json.loads(w.read())
+
+users = []
+addresses = []
+
+for user in userList:
+    name = user.get_full_name()
+    cell = user.get_cell()
+    email = user.get_email()
+    password = user.get_password()
+    newUser = model.Employee(employee_name = name, employee_cell = cell, employee_email=email, employee_password=password)
+    users.append(newUser)
+
+    coordinates = random_address.real_random_address_by_state('CA')
+    lat = coordinates["coordinates"]["lat"]
+    lng = coordinates["coordinates"]["lng"]
+    address = model.Address(lat=lat, lng=lng)
+    addresses.append(address)
+
+orders = []
+for order in order_data:
+    order_status = order["order_status"]
+    employee_id = order["employee_id"]
+    warehouse_id = order["warehouse_id"]
+    order = model.Order(order_status=order_status, employee_id=employee_id, warehouse_id=warehouse_id)
+    orders.append(order)
+
+warehouses = []
+for warehouse in warehouse_data:
+    employee_id = warehouse["employee_id"]
+    order_id = warehouse["order_id"]
+    warehouse_location = warehouse["warehouse_location"]
+    warehouse = model.Warehouse(employee_id=employee_id, order_id=order_id, warehouse_location=warehouse_location)
+    warehouses.append(warehouse)
+
+
+
+
+
+
+
+model.db.session.add_all(users)
+model.db.session.commit()
+
+model.db.session.add_all(addresses)
+model.db.session.commit()
+
+model.db.session.add_all(orders)
+model.db.session.commit()
+
+model.db.session.add_all(warehouses)
+model.db.session.commit()
+
