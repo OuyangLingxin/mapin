968d9b44a1b197042e82937770e4e1ad8b4a9c7d
OuyangLingxin <ouyangmayi@gmail.com>
2023-10-05 22:00:16 -0400

    "modified file formats"

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from server import app
+from model import connect_to_db
 
 class FlaskTests(TestCase):
   def setUp(self):
@@ -7,16 +8,16 @@ class FlaskTests(TestCase):
     app.config['TESTING'] = True
 
   def test1(self):
-    result = self.client.get('/maps')
-    self.assertEqual(result.status_code, 302)
-    self.assertIn(b'Add New Employee', result.data)
+    result = self.client.get('/', follow_redirects=True)
+    self.assertEqual(result.status_code, 200)
 
   def test2(self):
     result = self.client.post('/',
                       data={'email':'11@test.test', 'password': '123456789'},
                       follow_redirects=True)
-    self.assertIn(b'<a>Add New Employee</a>', result.data)
+    self.assertIn(b'<div\n  id="map"\n  class="m-auto object-fit-scale border rounded mb-4"\n  heigh="90%"\n></div>', result.data)
 
 if __name__ == '__main__':
   import unittest
+  connect_to_db(app)
   unittest.main()
